980d1564607eeaeaed4b6be87cc788b3c917b4fc
Piotr Ładoński <piolado@gmail.com>
2019-01-14 00:33:19 +0200

    Code autoformat according to new rules

--- src/Game.ts
+++ src/Game.ts
@@ -3,27 +3,27 @@ import { StartScene } from "./scenes/StartScene";
 
 // main game configuration
 const config: GameConfig = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	parent: "game",
-	scene: StartScene,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
-	}
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: "game",
+    scene: StartScene,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 200 }
+        }
+    }
 };
 
 // game class
 export class Game extends Phaser.Game {
-	constructor(config: GameConfig) {
-		super(config);
-	}
+    constructor(config: GameConfig) {
+        super(config);
+    }
 }
 
 // when the page is loaded, create our game instance
 window.addEventListener("load", () => {
-	var game = new Game(config);
+    const game = new Game(config);
 });
